fdd3c8a25c4404ef2892ac5c175fb3b9b979c4fd
Daniel Castillo <muzakryder2@msn.com>
2024-12-25 20:12:49 -0600

    initial commit. add DB and product card

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { APP_NAME } from '@/lib/constants'
+import Menu from './menu'
+
+function Header() {
+  return (
+    <header className='w-full border-b wrapper flex-between'>
+      <div className='flex-start'>
+        <Link href='/' className='flex-start'>
+          <Image
+            src='/images/logo.svg'
+            alt={`${APP_NAME} logo`}
+            height={48}
+            width={48}
+            priority={true}
+          />
+          <span className='hidden font-bold text-2xl ml-3 lg:block'>
+            {APP_NAME}
+          </span>
+        </Link>
+      </div>
+      <Menu />
+    </header>
+  )
+}
+
+export default Header
---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,22 @@
+'use server'
+
+import { prisma } from '@/db/prisma'
+import { convertToPlainObject } from '../utils'
+import { LATEST_PRODUCTS_LIMIT } from '../constants/index'
+
+// Get latest products
+export async function getLatestProducts() {
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: 'desc' },
+  })
+
+  return convertToPlainObject(data)
+}
+
+// get single product by its slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  })
+}
